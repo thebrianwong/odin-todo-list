9740a9c3ec3abc30998668ab8e891ec6d23db93b
Brian Wong <brianw7218@gmail.com>
2022-11-07 21:36:11 -0800

    Create foundation for to do item object

---
+++ src/todo_item.js
@@ -0,0 +1,45 @@
+const toDoItem = (title, description, dueDate, notes, checklist) => {
+    let pinned = false;
+    const getTitle = () => {
+        return title;
+    };
+    const setTitle = (newTitle) => {
+        title = newTitle;
+    };
+    const getDescription = () => {
+        return description;
+    };
+    const setDescription = (newDescription) => {
+        description = newDescription;
+    };
+    const getDueDate = () => {
+        return dueDate;
+    };
+    const setDueDate = (newDueDate) => {
+        dueDate = newDueDate;
+    };
+    const getNotes = () => {
+        return notes;
+    };
+    const setNotes = (newNotes) => {
+        notes = newNotes;
+    };
+    const getChecklist = () => {
+        return checklist;
+    };
+    const setChecklist = (newChecklist) => {
+        checklist = newChecklist;
+    };
+    const getPinned = () => {
+        return pinned;
+    };
+    const togglePinned = () => {
+        if (pinned) {
+            pinned = false;
+        } else {
+            pinned = true;
+        };
+    };
+    return { getTitle, setTitle, getDescription, setDescription, getDueDate, setDueDate,
+        getNotes, setNotes, getChecklist, setChecklist, getPinned, togglePinned };
+};
