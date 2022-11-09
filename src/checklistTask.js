99ac3b07a193b6f3ccf9110fd71fea4d58f204a2
Brian Wong <brianw7218@gmail.com>
2022-11-08 19:31:35 -0800

    Create module for checklist tasks

---
+++ src/checklistTask.js
@@ -0,0 +1,17 @@
+const checklistTaskObject = (description) => {
+    let completed = false;
+    const getTaskDescription = () => {
+        return description;
+    };
+    const setTaskDescription = (newDescription) => {
+        description = newDescription;
+    };
+    const toggleCompleted = () => {
+        if (completed) {
+            completed = false;
+        } else {
+            completed = true;
+        };
+    };
+    return { getTaskDescription, setTaskDescription, toggleCompleted };
+};
