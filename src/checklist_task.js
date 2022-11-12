8ac6446aa78cddaf36f13e69a9132b89d8a162b2
Brian Wong <brianw7218@gmail.com>
2022-11-11 20:54:54 -0800

    Add commented out that wasn't included earlier

--- src/checklist_task.js
+++ src/checklist_task.js
@@ -3,12 +3,12 @@ import { completedBehaviorComponent } from "./completed_component";
 // original where object would need to have Object.assign() for every instance
 // keeping for posterity, probably will deleted at the end
 // const checklistTaskObject = (description) => {
-//     const getTaskDescription = () => {
-//         return description;
-//     };
-//     const setTaskDescription = (newDescription) => {
-//         description = newDescription;
-//     };
+    // const getTaskDescription = () => {
+    //     return description;
+    // };
+    // const setTaskDescription = (newDescription) => {
+    //     description = newDescription;
+    // };
 //     return { getTaskDescription, setTaskDescription };
 // };
 
